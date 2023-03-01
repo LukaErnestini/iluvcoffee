f74054847e7f3e2edf19b7f368a3028540f7d008
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-01 11:57:43 +0100

    set up validation

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,5 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+// PartialType marks all the fields as optional
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
