cdf2c99f2969a886c6446ec8b5b2a72687f836b5
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-02 10:32:32 +0100

    fix imports for typo

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
-import { Coffee } from './entitites/coffee.entity';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee])],
